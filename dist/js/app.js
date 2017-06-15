5a8c9ff041a9d7a23844e150fe8df0a5ecf4d248
Felipe Gangrel <felipefrog@gmail.com>
2017-06-15 11:42:27 -0300

    design responsivo

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,30 +1,36 @@
 'use strict';
 
 $(document).ready(function () {
-    console.log('Ready to rock');
+  console.log('Ready to rock');
 
-    new WOW().init();
+  new WOW().init();
 
-    $('[data-scroll]').on('click', function (event) {
-        var rolarPara = $(this).data('scroll');
-        console.log(rolarPara);
+  $('[data-scroll]').on('click', function (event) {
+    var rolarPara = $(this).data('scroll');
+    console.log(rolarPara);
 
-        if ($(rolarPara)[0]) {
-            $('html, body').animate({
-                scrollTop: $(rolarPara).offset().top - 60
-            }, 1000);
-        }
+    if ($(rolarPara)[0]) {
+      $('html, body').animate({
+        scrollTop: $(rolarPara).offset().top - 60
+      }, 1000);
+    }
 
-        event.preventDefault();
-    });
+    event.preventDefault();
+  });
 
-    var posicaoLimiar = $('#sub-start').offset().top;
+  $('[menu-toggle]').on('click', function (event) {
+    console.log('clicou');
+    $('section#navbar .menu').toggleClass('toggle');
+    $(this).toggleClass('toggle');
+  });
 
-    $(window).scroll(function (event) {
-        if ($(window).scrollTop() > posicaoLimiar) {
-            $("#navbar").addClass('toggle');
-        } else {
-            $("#navbar").removeClass('toggle');
-        }
-    });
+  var posicaoLimiar = $('#sub-start').offset().top;
+
+  $(window).scroll(function (event) {
+    if ($(window).scrollTop() > posicaoLimiar) {
+      $("#navbar").addClass('toggle');
+    } else {
+      $("#navbar").removeClass('toggle');
+    }
+  });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -16,6 +16,11 @@ $(document).ready(()=>{
       event.preventDefault();
   });
 
+  $('[menu-toggle]').on('click', function(event){
+    console.log('clicou');
+    $('section#navbar .menu').toggleClass('toggle');
+    $(this).toggleClass('toggle');
+  });
   
   let posicaoLimiar = $('#sub-start').offset().top;
 
